221046680214a048b93894e6e1aa0863193912e9
danielw-007 <daniel030@binus.ac.id>
2023-11-30 13:32:38 +0700

    Initial Commit

---
+++ Network Calls/Network_CallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Network_CallsApp.swift
+//  Network Calls
+//
+//  Created by Daniel Widjaja on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Network_CallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
